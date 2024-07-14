6457acfe992647bf7465dfa547f4219dad00ebb5
X <i@jex.me>
2024-07-14 16:17:14 +0800

    Update deps

--- examples/server.ts
+++ examples/server.ts
@@ -92,7 +92,7 @@ async function serveDist(url: URL, req: Request, notFound: (url: URL, req: Reque
             const text = new TextDecoder().decode(chunk);
             if (text.includes("from \"typescript\"")) {
               controller.enqueue(new TextEncoder().encode(
-                text.replace("from \"typescript\"", "from \"https://esm.sh/typescript@5.5.2\""),
+                text.replace("from \"typescript\"", "from \"https://esm.sh/typescript@5.5.3\""),
               ));
               replaced = true;
             } else {
