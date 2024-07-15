2e6d0cd32e0b1bf1a0c0643139f12415c89d3449
X <i@jex.me>
2024-07-16 00:57:44 +0800

    patch: add keybindings for quick pick navigation on macOS

--- scripts/patch.ts
+++ scripts/patch.ts
@@ -16,3 +16,17 @@ const arr = js.split("this.storageService = storageService;");
 if (arr.length === 2) {
   await Deno.writeTextFile(path, arr[0] + "// Added by esm-monaco\nthis.storageService = " + commandHistoryStorage + arr[1]);
 }
+
+// add keybindings for quick pick navigation on macOS
+const registerQuickPickCommandAndKeybindingRules = `
+// Added by esm-monaco
+if (isMacintosh) {
+  registerQuickPickCommandAndKeybindingRule({ id: 'quickInput.Next', primary: 256 /* KeyMod.WinCtrl */ + 44 /* KeyCode.N */, handler: focusHandler(QuickPickFocus.Next) });
+  registerQuickPickCommandAndKeybindingRule({ id: 'quickInput.Previous', primary: 256 /* KeyMod.WinCtrl */ + 46 /* KeyCode.P */, handler: focusHandler(QuickPickFocus.Previous) });
+}
+`;
+const path2 = "node_modules/monaco-editor-core/esm/vs/platform/quickinput/browser/quickInputActions.js";
+const js2 = await Deno.readTextFile(path2);
+if (!js2.includes(registerQuickPickCommandAndKeybindingRules)) {
+  await Deno.writeTextFile(path2, js2 + registerQuickPickCommandAndKeybindingRules);
+}
