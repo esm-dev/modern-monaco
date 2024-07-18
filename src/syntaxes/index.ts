570fa5fd82c2cb8bb03e62037e29ff68b08d0329
X <i@jex.me>
2024-07-18 12:15:31 +0800

    Update syntaxes

--- src/syntaxes/index.ts
+++ src/syntaxes/index.ts
@@ -1,9 +1,9 @@
-import jsonScriptTag from "./(html)json-script-tag.json" with { type: "json" };
-import inlineHtml from "./(js)inline-html.json" with { type: "json" };
-import inlineCSS from "./(js)inline-css.json" with { type: "json" };
+import htmlJsonScriptTag from "./(html)json-script-tag.json";
+import inlineHtml from "./(js)inline-html.json";
+import inlineCSS from "./(js)inline-css.json";
 
 export default [
-  jsonScriptTag,
+  htmlJsonScriptTag,
   inlineHtml,
   inlineCSS,
 ];
