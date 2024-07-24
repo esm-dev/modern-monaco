92a711d6be0870ee4f884a682d3d2cf2d1cd7f15
X <i@jex.me>
2024-07-24 20:05:44 +0800

    Update examples

--- examples/vfs.js
+++ examples/vfs.js
@@ -9,47 +9,47 @@ const importMap = {
 };
 
 export const files = {
-  "log.d.ts": [
-    "/** log a message. */",
-    "declare function log(message:string): void;",
-  ].join("\n"),
-  "greeting.ts": "export const message = \"Hello world!\" as const;",
-  "index.html": [
-    "<!DOCTYPE html>",
-    "<html>",
-    "<head>",
-    "  <meta charset=\"utf-8\">",
-    "  <title>React App</title>",
-    "  <link rel=\"stylesheet\" href=\"./style.css\">",
-    "  \<script type=\"importmap\">",
-    JSON.stringify(importMap, null, 2).split("\n").map((line) => "  " + line).join("\n"),
-    "  <\/script>",
-    "</head>",
-    "<body>",
-    "  <div id=\"root\"></div>",
-    "  <script type=\"module\" src=\"./main.jsx\"><\/script>",
-    "</body>",
-    "</html>",
-  ].join("\n"),
-  "style.css": [
-    "h1 {",
-    "  font-style: italic;",
-    "}",
-  ].join("\n"),
-  "App.tsx": $APP_TSX,
-  "main.jsx": [
+  "src/greeting.ts": "export const message = \"Hello world!\" as const;",
+  "src/App.tsx": $APP_TSX,
+  "src/main.jsx": [
     "import { createRoot } from \"react-dom/client\"",
     "import App from \"./App.tsx\"",
     "",
     "const root = createRoot(document.getElementById(\"root\"))",
     "root.render(<App />)",
   ].join("\n"),
+  "style/style.css": [
+    "h1 {",
+    "  font-style: italic;",
+    "}",
+  ].join("\n"),
+  "types/log.d.ts": [
+    "/** log a message. */",
+    "declare function log(message:string): void;",
+  ].join("\n"),
+  "index.html": [
+    "<!DOCTYPE html>",
+    "<html>",
+    "<head>",
+    "  <meta charset=\"utf-8\">",
+    "  <title>React App</title>",
+    "  <link rel=\"stylesheet\" href=\"./style/style.css\">",
+    "  \<script type=\"importmap\">",
+    JSON.stringify(importMap, null, 2).split("\n").map((line) => "  " + line).join("\n"),
+    "  <\/script>",
+    "</head>",
+    "<body>",
+    "  <div id=\"root\"></div>",
+    "  <script type=\"module\" src=\"./src/main.jsx\"><\/script>",
+    "</body>",
+    "</html>",
+  ].join("\n"),
   "import_map.json": JSON.stringify(importMap, null, 2),
   "tsconfig.json": JSON.stringify(
     {
       compilerOptions: {
         types: [
-          "log.d.ts",
+          "types/log.d.ts",
           "https://raw.githubusercontent.com/vitejs/vite/main/packages/vite/types/importMeta.d.ts",
         ],
       },
@@ -62,6 +62,6 @@ export const files = {
 export const vfs = new VFS({
   scope: "test",
   initial: files,
-  defaultFile: 'index.html',
+  defaultFile: "index.html",
   history: "browserHistory"
 });
