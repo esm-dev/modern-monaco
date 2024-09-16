28301f54a32fe4956eb6abff9208ddca3060af63
Je Xia <i@jex.me>
2024-09-16 19:20:46 +0800

    scripts/build: use esbuild 0.32.1

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,4 @@
-import { build as esbuild } from "npm:esbuild@0.23.0";
+import { build as esbuild } from "https://deno.land/x/esbuild@v0.23.1/mod.js";
 import { grammars as tmGrammars } from "../node_modules/tm-grammars/index.js";
 import { themes as tmThemes } from "../node_modules/tm-themes/index.js";
 import { wasmBinary } from "../node_modules/@shikijs/core/dist/wasm-inlined.mjs";
