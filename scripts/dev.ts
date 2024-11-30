b1b534e92d231be9e112378380f1ec360a099ff0
Je Xia <i@jex.me>
2024-11-30 20:16:33 +0800

    Use shikijs 1.24.0

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -31,7 +31,7 @@ async function serveDist(url: URL, req: Request) {
             const text = new TextDecoder().decode(chunk);
             if (text.includes("from \"typescript\"")) {
               controller.enqueue(new TextEncoder().encode(
-                text.replace("from \"typescript\"", "from \"https://esm.sh/typescript@5.5.3\""),
+                text.replace("from \"typescript\"", "from \"https://esm.sh/typescript@5.7.2\""),
               ));
               replaced = true;
             } else {
