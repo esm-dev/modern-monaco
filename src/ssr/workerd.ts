cc31c2c278da6b40a45041c21f02039508a645c2
Je Xia <i@jex.me>
2025-12-03 09:18:51 +0800

    Clean up

--- src/ssr/workerd.ts
+++ src/ssr/workerd.ts
@@ -1,8 +1,7 @@
 // ! external modules, don't remove the `.js` extension
 import { setDefaultWasmLoader } from "../shiki.js";
 
-// cloudflare worker doesn't allow to import wasm module from binary
-// let's use `import()` to load the wasm module
+// Set the default wasm loader for Cloudflare Workerd
 // @ts-expect-error the 'onig.wasm' is created at build time
 setDefaultWasmLoader(import("../onig.wasm"));
 
